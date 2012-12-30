5f3c745127c81b4e86a2fb502351afa458fd02b5
Jonathan Zhu <jonathanzhu@ubuntu.(none)>
2012-12-30 00:57:43 -0500

    Jonathan's First Commit! Understanding Controllers

---
+++ siteAPI/application/views/jonathan.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>application/css/style.css">
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>Hello World! :DDDDDD </p>
+
+		
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
