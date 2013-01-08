297f14cc8f8420b33ad8454ec62255eb209874ca
Owner <joshcai@ubuntu.(none)>
2013-01-08 02:32:53 -0600

    Update functions for users added in membership_model (still needs new password change)

--- siteAPI/application/models/membership_model.php
+++ siteAPI/application/models/membership_model.php
@@ -34,6 +34,45 @@ class Membership_model extends CI_Model {
                 $insert = $this->db->insert('users', $new_member_insert_data);
                 return $insert;
         }
+        
+        function update_user($name,$email,$password,$old_password)
+        {
+        	if(!$this->Common->check_pass($old_password))
+        	{
+
+				return -1;
+			}
+			
+        	$data = array('email' => $email, 'name' => $name);
+			$this->db->where('id', $this->Common->user_id());
+			$this->db->update('users', $data); 	
+        }
+        
+        function update_address($address)
+        {
+
+        		$data = array('address' => $address->address, 'def' => $address->def);
+				$this->db->where('id', $addr->id);
+				$this->db->where('user_id', $this->Common->user_id()); //don't actually need this line of code
+				$this->db->update('address', $data); 	
+
+        }
+        
+        function website($website){
+
+        		$data = array('url' => $website->url, 'def' => $website->def);
+				$this->db->where('id', $website->id);
+				$this->db->where('user_id', $this->Common->user_id()); //don't actually need this line of code
+				$this->db->update('website', $data); 	
+       
+        }
+        
+        function phone($phone){
+        		$data = array('numbers' => $phone->numbers, 'def' => $phone->def);
+				$this->db->where('id', $phone->id);
+				$this->db->where('user_id', $this->Common->user_id()); //don't actually need this line of code
+				$this->db->update('phone', $data); 	     
+        }
 }
 
 
