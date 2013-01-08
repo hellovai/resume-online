6bd5f7b779d8a27584819677a2230ddbcf1419b8
Owner <joshcai@ubuntu.(none)>
2013-01-08 02:00:37 -0600

    Cover and Cover_model added

---
+++ siteAPI/application/controllers/cover.php
@@ -0,0 +1,41 @@
+<?php
+
+class Cover extends CI_Controller 
+{
+	//function __construct()
+	//{
+		
+	//}
+	
+	function index()
+	{
+		$this->load->model('cover_model');
+		$data['titles'] = $this->Cover_model->get_covers();
+		
+		$data['cover'] = $this->Cover_model->get_info($this->input->post('cover_id'));
+		
+		
+		$data['context'] = 'cover_letter';
+		$this->load->view('template/main', $data);
+	}
+
+	function save()
+	{
+		$this->load->model('cover_model');
+
+		$this->Cover_model->save_text($this->input->post('id'),$this->input->post('info'));
+		$this->Cover_model->save_title($this->input->post('id'),$this->input->post('title'));
+		$data['success'] = "Your cover letter was saved!";
+		$this->index();
+	}
+	
+	function create()
+	{
+		$this->load->model('cover_model');
+		$this->Cover_model->create($this->input->post('title'));
+
+		$this->index();
+	}
+
+
+}
---
+++ siteAPI/application/models/cover_model.php
@@ -0,0 +1,64 @@
+<?php
+
+class Cover_model extends CI_Model {
+
+        function get_covers($max = false)
+        {
+        	$this->db->order_by("updated", "desc");
+        	$this->db->where('user_id', $this->session->userdata('user_id')); 
+        	$this->db->select('id, title, updated');
+        	if($max === false)
+        	{
+				$query = $this->db->get('cover_letter');  
+			}
+			else
+			{
+				$query = $this->db->get('cover_letter',$max);  
+			}
+				
+			   
+            return $query->result();
+        }
+        function get_info($cover_id = NULL)
+        {
+
+        	if(!isset($cover_id))
+        	{
+        		$this->db->order_by("updated", "desc");
+        	}
+        	else
+        	{
+        		$this->db->where('id', $cover_id); 
+        	}
+        	$this->db->where('user_id', $this->session->userdata('user_id'));
+        	$this->db->select('id,title,updated,info');
+        	$query = $this->db->get('cover_letter',1); 
+        	
+            
+        	return $query->result();
+        }
+        
+        function save_title($cover_id,$title)
+        {
+        	$data = array('title' => $title);
+			$this->db->where('id', $cover_id);
+			$this->db->update('cover_letter', $data); 
+        }
+        
+        function save_text($cover_id,$info)
+        {
+        	$data = array('info' => $info);
+   			$this->db->where('id', $cover_id);
+   			$this->db->set('updated', 'NOW()', FALSE);  	  
+			$this->db->update('cover_letter', $data);   
+        }
+        
+        function create($title)
+        {
+        	$data = array('title' => $title);
+			$this->db->set('created', 'NOW()', FALSE);
+			$this->db->set('updated', 'NOW()', FALSE);
+			$this->db->insert('cover_letter', $data); 
+        }
+
+}
