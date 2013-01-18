00c3cf03571d2296971f54b21446d125243bd226
Vaibhav Gupta <kacxdak@gmail.com>
2013-01-18 15:01:35 -0600

    updated some stuff, forgot what

--- siteAPI/application/controllers/contact.php
+++ siteAPI/application/controllers/contact.php
@@ -3,21 +3,28 @@
 class contact extends CI_Controller {
 
 	function send() {
-		$config['smtp_host'] = 'ssl://smtp.googlemail.com';
-		$config['smtp_port'] = 465;
-		$config['smtp_user'] = 'lastnameitry@gmail.com';
-		$config['smtp_pass'] = 'vaibhavjoshjonathan';
+	
+		$config = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_port' => 465,
+			'smtp_user' => 'lastnameitry@googlemail.com',
+			'smtp_pass' => 'vaibhavjoshjonathan',
+		);
 		
 		$this->load->library('email', $config);
-
-		$this->email->from($this->input->post('email'), $this->input->post('name'));
+	
+		
+		$this->email->from('lastnameitry@gmail.com', 'Jeffrey Way');
+		
+		$this->email->reply_to($this->input->post('email'), $this->input->post('name'));
 		$this->email->to('kacxdak@gmail.com');
 
 		$this->email->subject('Resume Builder: ' . $this->input->post('subject'));
 		$this->email->message($this->input->post('message'));	
 
 		if($this->email->send()) 
-		redirect('welcome');
+			redirect('welcome');
 		else
 			show_error($this->email->print_debugger());
 	}
